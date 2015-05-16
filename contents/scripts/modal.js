ba61428ba599f6668d80079df862bd2cf06437c5
Amber Roche <rochea@amazon.com>
2015-05-15 20:50:06 -0700

    angularjs

--- contents/scripts/modal.js
+++ contents/scripts/modal.js
@@ -1,3 +1,10 @@
 
+angular.module('Brussels', [])
+	.controller('ModalCtrl', function() {
+		console.log('modal ctrl');
+	})
+;
+
+
 openModal = function(file) { console.log('open modal with file: '+ file); }
 
